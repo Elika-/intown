65e3b86deb44a00058d001abe3a0fd51136d6918
Sumit Datta <sumit.datta@smaato.com>
2015-05-24 00:34:37 +0200

    Delete data on server start

--- feeds/flickr.js
+++ feeds/flickr.js
@@ -24,16 +24,12 @@ exports.fetch = function (city, redis) {
 	https.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=98e130ea926d426413730b9018fcbe65&" +
 		"format=json&nojsoncallback=1&per_page=10&extras=date_taken,owner_name,url_m&min_taken_date=" + moment().subtract(1, 'days').unix() +
 		"&text=" + city, function(res) {
-		console.log("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=98e130ea926d426413730b9018fcbe65&" +
-		"format=json&nojsoncallback=1&per_page=10&min_taken_date=" + moment().subtract(1, 'days').unix() +
-		"&text=" + city);
-		// console.log('STATUS: ' + res.statusCode);
-		// console.log('HEADERS: ' + JSON.stringify(res.headers));
-		// res.setEncoding('utf8');
+
 		body = '';
 		res.on('data', function (chunk) {
 			body += chunk;
 		});
+
 		res.on('end', function() {
 			console.log(JSON.parse(body));
 			var data = JSON.parse(body).photos.photo;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var app = require('express')();
 var http = require('http');
-var io = require("socket.io")();
 var path = require('path');
 
 var flickr = require('./feeds/flickr');
@@ -19,51 +18,54 @@ app.set('port', process.env.PORT || 3000);
 
 server = http.Server(app);
 server.listen(app.get('port'), function() {
+	// Clean existing data from Redis
+	client.keys('*', function(err, replies) {
+		if (!err && replies.length) {
+			client.del(replies, function(err, res) {});
+		}
+	});
+
 	var minutes = 1, the_interval = minutes * 60 * 1000;
-	//fetch_data();
-//	setInterval(fetch_data, the_interval);
+	// fetch_data();
+	// setInterval(fetch_data, the_interval);
 });
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname+'/app/index.html');
+	res.sendFile(__dirname + '/app/index.html');
 });
 
-app.get('/:city', function(req,res) {
+app.get('/:city', function(req, res) {
 	var queryCity = req.params.city;
-	fetch_single(queryCity);
-	var resp =""
-	client.zrange(["data-"+queryCity, 0, -1], function(err, reply) {
-		for(i = 1; i < reply.length; i++) {
-			resp += reply[i];
-
-		}
-		res.end(JSON.stringify(reply));
-	});
-
-		
+	if (queryCity == 'favicon.ico') {
+		res.end();
+	} else {
+		fetch_single(queryCity);
+		var resp = []
+		client.zrange(["data-" + queryCity, 0, -1], function(err, reply) {
+			for(i = 1; i < reply.length; i++) {
+				resp.push(JSON.parse(reply[i]));
+			}
+			res.json(reply);
+		});
+	}
 });
 
 function fetch_single(city) {
 		console.log("fetch data for "+city);
-		//flickr.fetch(city, http, client);
-		twitter.fetch(city, client)
-		bandsInTown.fetch(city,http,client);
-
+		flickr.fetch(city, client);
+		twitter.fetch(city, client);
+		// bandsInTown.fetch(city, client);
 }
 
 function fetch_data() {
 	console.log("fetch_data");
 	client.keys('data-*', function(err, reply) {
-
 		for(i = 0; i < reply.length; i++) {
 			var city = getPlainName(reply[i]);
 			fetch_single(city);
 		}
-
 	});
 }
-
-
 
 function getPlainName(key) {
 	return key.substring(5);

f5a22cbc99697ed86d7c10d1a4377b5268697cc8
Mirco <ablx@posteo.de>
2015-05-24 12:12:46 +0200

    query all 10 secs

--- app/static/intown.js
+++ app/static/intown.js
@@ -6,18 +6,22 @@ $(function() {
 
     e.preventDefault();
 
+
+    setInterval( function() {
     $.getJSON('/' + city, function(data) {
-      console.log(data.length);
+      console.log("======");
       txt ="";
+      $('#content').html('');
       for(i = 0; i < data.length; i++) {
 
       $('#content').append("<div>");   
       $('#content').append("<h4>"+data[i].title+"</h4>");
-      $('#content').append("<h4>"+data[i].title+"</h4>");   
+    //  $('#content').append("<h4>"+data[i].title+"</h4>");   
       $('#content').append("<div>");      
       }
 
     })
+  }, 10000);
   });
 
   $('.sources').on('click', function(e) {
